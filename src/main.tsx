fca9fc736d3b7a109274f934011e60dbda6c173d
Bastien Pailloux <bastien.pailloux@hotmail.fr>
2025-03-24 16:50:18 +0100

    fix: Suppressiond es import React inutile

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 // En utilisant jsx: 'react' nous devons importer React explicitement
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
